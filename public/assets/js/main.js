706177d0ecac84d58f94c648c12d3095e75fe9be
Piqzaa <alex.berrel@gmail.com>
2026-05-25 08:09:22 +0200

    feat(js): create menu module for mobile interactions

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,8 +3,10 @@ import { initClients } from "./modules/clients.js";
 import { initDevis } from "./modules/devis.js";
 import { initFactures } from "./modules/factures.js";
 import { initDashboard } from "./modules/dashboard.js";
+import { initMenu } from "./modules/menu.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  initMenu();
   initModal();
   initClients();
   initDevis();
